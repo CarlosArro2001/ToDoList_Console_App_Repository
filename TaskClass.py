1cc20e3889cd2da7cd01177d3111f74f0547951c
CarlosArro2001 <carlosarrowork@gmail.com>
2021-02-06 11:28:13 +0300

    Final touches
    Made sure the all the functions are working properly

--- TaskClass.py
+++ TaskClass.py
@@ -9,35 +9,33 @@ class TaskClass():
     x = datetime.datetime.now().date()
     #getter method for tasks 
     def getTasks(self):
-        with open(csv_File,'r') as file:
+        with open(csv_File,encoding='utf-8-sig') as file:
             reader=csv.reader(file,delimiter=',')
             for row in reader: 
                 if(not row):
                     continue
                 else:
                     temp_Tasks.append(row)
-            if(temp_Tasks[0][0]=='ï»¿No '):
-                temp_Tasks.remove(temp_Tasks[0])
-            i = 0
+            i = 0 
             while(i <len(temp_Tasks)):
                 print("\n {0}. {1} \n notes: \n{2}".format(temp_Tasks[i][0],temp_Tasks[i][1],temp_Tasks[i][2]))
                 i = i+1    
     #setter method for tasks
-        def setTasks(self):
-            #getting amount of rows in csvfile
-            with open(csv_File,'r+') as file:
-                reader_file = csv.reader(file,delimiter=',')
-                value = len(list(reader_file))
-                print(value)
-            #t_no will now contain the value of the task t_no
-            t_No = str(value-1) # value - 1 because the lines add by 1
+    def setTasks(self):
+        #getting amount of rows in csvfile
+        with open(csv_File,'r+') as file:
+            reader_file = csv.reader(file,delimiter=',')
+            value = len(list(reader_file))
+            print(value)
+        #t_no will now contain the value of the task t_no
+            t_No = str(value) # value - 1 because the lines add by 1
             print("Enter the task name :" )
             t_Name = input()
             print("Enter any notes :" )
             t_Notes = input()
-            with open(csv_File,'a') as file:
-                writer = csv.writer(file)
-                writer.writerow([t_No,t_Name,t_Notes])
+        with open(csv_File,'a') as file:
+            writer = csv.writer(file)
+            writer.writerow([t_No,t_Name,t_Notes])
         #deleting method for tasks 
         #get the values in a list , delete a value from the list , write to the value 
     def deleteTasks(self):
